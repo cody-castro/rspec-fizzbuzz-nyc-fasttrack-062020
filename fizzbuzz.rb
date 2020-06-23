c93673d9fd24e8f525f9d2d2e6a21bcd1ec1e4a6
Cody C <codyicastro@gmail.com>
2020-06-23 21:12:20 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,3 +1,7 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 
+# We expect fizzbuzz(3) to return "Fizz"
+# We expect fizzbuzz(5) to return "Buzz"
+# We expect fizzbuzz(15) to return "FizzBuzz"
+
