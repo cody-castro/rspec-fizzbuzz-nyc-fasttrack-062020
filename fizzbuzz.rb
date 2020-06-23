76dd1494738458daa68dcb2693a9b46511e519d8
Cody C <codyicastro@gmail.com>
2020-06-23 21:13:35 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -4,6 +4,7 @@
 # We expect fizzbuzz(3) to return "Fizz"
 # We expect fizzbuzz(5) to return "Buzz"
 # We expect fizzbuzz(15) to return "FizzBuzz"
+# We expect fizzbuzz(4) to return nil
 
 def fizzbuzz(num)
   if num%3 == 0 && num%5 == 0
